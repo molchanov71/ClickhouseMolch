f3dd70d22b28a4785753d49921ac59a044e65cc5
Молчанов Степан Андреевич <molchanov.sa@foamline.com>
2023-07-26 10:35:07 +0300

    real sql query

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import clickhouse_connect
 
 def main():
     client = clickhouse_connect.get_client(host='localhost', port=8123)
-    res = client.query('SELECT block_foam_type FROM `default`.NewTable')
-    print(*{tpl[0] for tpl in res.result_rows}, sep='\n')
+    res = client.query('SELECT DISTINCT block_foam_type FROM `default`.NewTable')
+    print(*(tpl[0] for tpl in res.result_rows), sep='\n')
 
 
 if __name__ == '__main__':
